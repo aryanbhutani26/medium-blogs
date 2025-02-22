9c47b94451c775b031e56b3ffab9a30ffb6bf304
aryanbhutani26 <159881202+aryanbhutani26@users.noreply.github.com>
2025-02-22 21:44:17 +0530

    1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,9 +10,12 @@ function App() {
 
   return (
     <>
+    
+  
       <BrowserRouter>
       <Routes>
       <Route path="/" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/blog/:id" element={<Blog />} />
